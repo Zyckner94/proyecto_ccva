56608eccc268b2a71b00d08b56b6bcd140e90b08
Martin <martin_e.a@hotmail.com>
2022-08-06 13:31:33 -0300

    agregado app de blog con sus respectivos campos

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,35 @@
+from contextlib import nullcontext
+from distutils.command.upload import upload
+from email.mime import image
+from os import truncate
+from tokenize import blank_re
+from turtle import title, update
+from unicodedata import category
+from venv import create
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=250)
+    content = models.TextField()
+    slug = models.SlugField(max_length=250, unique=True)
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+    #image = models.ImageField(upload_to = 'images/', blank=True, null=True)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    content = models.TextField()
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+    
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+
+class Category(models.Model):
+    nombre = models.CharField(max_length=250)
